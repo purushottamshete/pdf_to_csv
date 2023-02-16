d8d93b6dd7ea3da4bf768ad6aa99391205e3b66e
Purushottam <purushottamshete@gmail.com>
2023-02-16 15:57:35 +0530

    removing the comments

--- main.py
+++ main.py
@@ -21,46 +21,33 @@ tour = {}
 for row_num, page in enumerate(pages):
     lines = page.split('\n')
     for col_num, line in enumerate(lines):
-        #sheet.cell(row=row_num+1, column=col_num+1, value=line)
-        #print(line)
         day_line = re.search('Day [0-9]$', line)
         if day_line:
-            #print(day_line.string)
             day_num = day_line.string.split(' ')[2]
-            #print(f'Day: {day_num}')
             tour['Day'] = day_num
 
         tour_line = re.search('^Tour', line)
         if tour_line:
-            #print(tour_line.string)
             tour_name = tour_line.string.split(' ')[2:]
-            #print(f"Tour Name: {' '.join(tour_name)}")
             tour['Tour Name'] = " ".join(tour_name)
             
         start_time_line = re.search('Start Time:', line)
         if start_time_line:
-            #print(start_time_line.string.split(' '))
             start_time = start_time_line.string.split(' ')[3]
-            #print(f"Start Time: {start_time}")
             tour['Start Time'] = start_time
 
         end_time_line = re.search('End Time:*', line)
         if end_time_line:
-            #print(end_time_line.string)
             end_time = end_time_line.string.split(' ')[3]
-            #print(f"End Time: {end_time}")
             tour['End Time'] = end_time
 
         duration_line = re.search('Duration:*', line)
         if duration_line:
-            #print(duration_line.string)
             duration= duration_line.string.split(' ')[2]
-            #print(f"Duration: {duration}")
             tour['Duration'] = duration
 
 
         if len(tour) == 5:
-            #print('writing to to the list')
             tour = {
                 "Day": day_num,
                 "Tour Name": tour_name,
